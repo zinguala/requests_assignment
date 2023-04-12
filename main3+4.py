a70ddef4b18a8b83c3268621934d144ea678fe91
Your Name <aviv.zinger@gmail.com>
2023-04-12 10:52:16 +0300

    Merge remote-tracking branch 'origin/Aviv_Singer' into Aviv_Singer

--- main3+4.py
+++ main3+4.py
@@ -14,7 +14,7 @@ class SpeedUser:
         return float(geo['lat'])
 
     def __str__(self):
-        return f'{self.__dict__}'
+        return f'name: {self.name}, username: {self.username}, address: {self.address} '
 
 
 user_objects = []
@@ -30,29 +30,33 @@ while True:
     else:
         break
 
-
-for i in range(len(user_objects)):
-    print(user_objects[i])
-
 while True:
     try:
-        user_input = int((input("enter lat to check if exists in database : ")))
+        user_input = float((input("enter lat to check if exists in database : ")))
     except ValueError:
         print('wrong input ,lets try again!')
         continue
 
-    counter = 0                              # counter for print if no matching user
+    counter = 0       # counter for print if no matching user
+
     for user in user_objects:                # for checking
         counter += 1
         lat = user.lat()
         if user_input == lat:
-            print(f'Matching lat found in the database {user.name}')
+            print(f'Matching lat found in the database {user.lat()}')
             print(user)
-            counter = 0
-        else:
-            nearest_lat = lat = user.lat()
+            counter = 1
+
+        elif counter == 1:
+            nearest_lat = user.lat()
+            nearest_user = user.__str__()
+
+        elif abs(user_input - user.lat()) < abs(user_input - nearest_lat):
+            nearest_lat = user.lat()
             nearest_user = user.__str__()
 
         if len(user_objects) == counter:
             print('no matching lat found in the database')
-            print(f'the nearest is : {nearest_user}')
+            print(f'the nearest is : {nearest_lat}')
+            print(user)
+
